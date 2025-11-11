568866e4b8e1c8a19b6cf5ba1f8d6e6074257a86
Vladislav Flinski <flinssski@gmail.com>
2025-11-11 13:17:58 +0300

    feat: add about page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import { Nunito_Sans } from 'next/font/google'
+import { Inter } from 'next/font/google'
 
 import Header from '@/components/Header'
 import '@/styles/globals.css'
 
-const nunitoSans = Nunito_Sans({
+const inter = Inter({
 	subsets: ['latin'],
 	display: 'swap',
 })
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
 			<body
-				className={`${nunitoSans.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative`}
+				className={`${inter.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative`}
 			>
 				<Header />
 				<div className="flex-1 px-8 py-12">
